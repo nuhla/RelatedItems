e29a1ebd6ae8ed2a306c039d596407f05157a6b6
nuhla al masri <45833345+nuhla@users.noreply.github.com>
2019-11-17 21:27:59 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+const path = require("path");
+const config = {
+  optimization: {
+    splitChunks: {
+      minSize: 300000,
+      cacheGroups: {
+        default: false
+      }
+    }
+  },
+  entry: ["react-hot-loader/patch", path.resolve(__dirname, "./src/app.jsx")],
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom"
+    }
+  },
+  devServer: {
+    contentBase: "./public"
+  }
+};
+module.exports = config;
