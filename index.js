5a292c7f2f2f60e0e04ef2c3ca61abb108d98b94
nuhla <nuhlamasri@gmail.com>
2020-01-27 16:10:11 +0200

    add comments

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
-const express = require("express");
-const path = require("path");
-const request = require("request");
+const express = require('express');
+const path = require('path');
+const request = require('request');
 const app = express();
-// const config = require(__dirname + "/config/keys");
 
-// requiring and using body parser to parse our req body
-var bodyParser = require("body-parser");
+//-----------------------------------------------------------------//
+//---- requiring and using body parser to parse our req body ------//
+//-----------------------------------------------------------------//
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// if the app is depoyed then the port is ganna be taken from the env ,else its 3003
+//--------------------------------------------------------------------------------------------------//
+// ----------if the app is depoyed then the port is ganna be taken from the env ,else its 3003 -----//
+//--------------------------------------------------------------------------------------------------//
 const port = process.env.PORT || 3005;
 
-/// this line is for te cross origin - prplem
+//----------------------------------------------------//
+//------------- solving cors proplem -----------------//
+//------------- allow Origin for All -----------------//
+//----------------------------------------------------//
+
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
-
-app.get("/getRate", (req, res) => {
+//------------------------------------------------------//
+//-------------access the review API  ------------------//
+//------------- and get the needed data from it---------//
+//------------------------------------------------------//
+app.get('/getRate', (req, res) => {
   console.log(req.query);
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   request(
-    "https://protected-plains-93575.herokuapp.com/reviewsApi/getRate/" +
+    'https://protected-plains-93575.herokuapp.com/reviewsApi/getRate/' +
       req.body.id,
 
     function(error, response, body) {
+      //--------------------------------------------------------------//
+      //------------- if the data sccesed sucssrfully ----------------//
+      //------------- we need to send it back to the component -------//
+      //--------------------------------------------------------------//
       if (!error && response.statusCode === 200) {
-        debugger;
-        console.log("rate", body);
         res.json(body);
       } else {
-        res.end("err:" + error);
+        res.end('err:' + error);
       }
     }
   );
 });
 
-/// define and use our static directores in the app
-app.use(express.static(path.join(__dirname, "public")));
+//------------------------------------------------------------------------------//
+//---------------- define and use our static directores in the app--------------//
+//------------------------------------------------------------------------------//
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join(__dirname, 'module-0')));
-app.use(express.static(path.join(__dirname, "node_nmodules")));
+app.use(express.static(path.join(__dirname, 'node_nmodules')));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // db.save({ description: "wyruu", updateDate: Date.now() });
 });
 // app.set('view engine', 'jsx')
